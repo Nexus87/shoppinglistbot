a93e5bbf275b6158dd84fea0adb911155cecc691
Nexuz <nexxuz0@googlemail.com>
2019-02-19 23:59:40 +0100

    Fix: Remove trailing whitespaces and don't add empty text

--- bot/src/handler/einkaufen_handler.rs
+++ bot/src/handler/einkaufen_handler.rs
@@ -18,14 +18,32 @@ impl EinkaufenCommandHandler {
     }
 }
 
+fn split_args(cmd_args: &str) -> Vec<String> {
+    cmd_args.split(';')
+        .map(|x| String::from(x.trim()))
+        .filter(|x| !x.is_empty())
+        .collect()
+}
+
 impl CommandHandler for EinkaufenCommandHandler {
     fn handle_message(&self, cmd_args: &str) {
-        let items: Vec<String> = cmd_args.split(';')
-            .map(String::from)
-            .collect();
+        let items = split_args(cmd_args);
         let future = self.api.add_tasks(&items, self.project_id)
             .map(|_| ())
             .map_err(|_| ());
         tokio::run(future);
     }
+}
+
+#[test]
+fn split_args_test() {
+    let args = "1; 4; 5 ; 6; ";
+    let items = split_args(args);
+    let expected = vec![
+        String::from("1"),
+        String::from("4"),
+        String::from("5"),
+        String::from("6")
+    ];
+    assert_eq!(items, expected);
 }
