2beef9ba66cc722aabe7db0fd03b5d85693576e6
Nexxuz0 <nexxuz0@googlemail.com>
2019-02-24 21:22:46 +0100

    Remove unused api

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -25,7 +25,6 @@ use services::get_telegram_service;
 
 
 fn main() {
-    let token = env::var("TELEGRAM_BOT_TOKEN").unwrap();
     let todoist_token = env::var("TODOIST_TOKEN").unwrap();
     let project_id: i64 = env::var("PROJECT_ID").unwrap().parse().unwrap();
     let db_path = "./my.db";
@@ -37,7 +36,6 @@ fn main() {
         .map(From::from)
         .collect();
 
-    let api = Api::configure(token).build().unwrap();
     let db = get_storage(&db_path);
     let telegram_message_service = get_telegram_service(todoist_token, project_id, client_ids, db);
 
